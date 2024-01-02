b08d2470a895d5828b9a7f9283143b2dc479df3c
Mustakim4 <mustakquimdiwan@gmail.com>
2024-01-02 08:04:52 +0530

    UI is complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,165 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container mt-3">
+    <div className="container mt-5">
       <div className="row justify-content-center">
-        <div className="col-4 allBorder">
+        <div className="col-4 card">
           <div className="row">
-            <div className="col">
-              <textarea name="" id="" cols="" rows=""></textarea>
+            <div className="col mt-3">
+              <textarea name="" id="" cols="" rows="3"></textarea>
             </div>
           </div>
 
           <div className="row">
-            <div className="col">1</div>
-            <div className="col">2</div>
-            <div className="col">3</div>
-            <div className="col">/</div>
-          </div>
+            <div className="col">
+              <div className="row">
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    1
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    2
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    3
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    /
+                  </button>
+                </div>
+              </div>
 
-          <div className="row">
-            <div className="col">4</div>
-            <div className="col">5</div>
-            <div className="col">6</div>
-            <div className="col">x</div>
-          </div>
+              <div className="row">
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    4
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    5
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    6
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
 
-          <div className="row">
-            <div className="col">7</div>
-            <div className="col">8</div>
-            <div className="col">9</div>
-            <div className="col">+</div>
-          </div>
+              <div className="row">
+                <div className="col-3">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    7
+                  </button>
+                </div>
+                <div className="col-3">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    8
+                  </button>
+                </div>
+                <div className="col-3">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    9
+                  </button>
+                </div>
+                <div className="col-3">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
 
-          <div className="row">
-            <div className="col">.</div>
-            <div className="col">0</div>
-            <div className="col">=</div>
-            <div className="col">-</div>
+              <div className="row">
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    .
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    0
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-success btn-size"
+                  >
+                    =
+                  </button>
+                </div>
+                <div className="col">
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary btn-size"
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div className="row text-center">
-            <div className="col">clear</div>
+            <div className="col mt-3">
+              <button type="button"  className="btn btn-outline-danger btn-size">
+                Clear
+              </button>
+            </div>
           </div>
         </div>
       </div>
